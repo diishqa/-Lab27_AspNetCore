3cad1848e77d8b2e57190dd5845a3ad404b4ed8b
alincqeek <pstartceva@yandex.ru>
2026-04-10 08:56:40 +0300

    Step 6: multiple routes with parameters

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Привет от ИСП-232 Автор: Диана!");
+app.MapGet("/", () => "Добро пожаловать на сервер!");
+app.MapGet("/about", () => "Это мой первый ASP.NET Core сервер");
+app.MapGet("/time", () => $"Время на сервере: {DateTime.Now}");
+app.MapGet("/hello/{name}", (string name) => $"Привет, {name}!");
 
 app.Run();
